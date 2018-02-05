2bd0f534f298116464cf615452082104b65715da
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2018-02-05 11:39:00 +0100

    The trivial rotatable manifold: a circle.

--- Math/Rotations/Class.hs
+++ Math/Rotations/Class.hs
@@ -10,7 +10,7 @@
 
 {-# LANGUAGE TypeFamilies             #-}
 
-module Math.Rotations.Class where
+module Math.Rotations.Class ( Rotatable (..) ) where
 
 import Math.Manifold.Core.Types
 import Data.VectorSpace
@@ -19,3 +19,14 @@ class Rotatable m where
   type AxisSpace m :: *
   rotateAbout :: AxisSpace m -> S¹ -> m -> m
 
+
+instance Rotatable S¹ where
+  type AxisSpace S¹ = ℝP⁰
+  rotateAbout ℝPZero (S¹ δφ) (S¹ φ)
+    | φ' > pi    = S¹ $ φ-tau
+    | φ' < -pi   = S¹ $ φ+tau
+    | otherwise  = S¹ φ'
+   where φ' = φ + δφ
+
+tau :: ℝ
+tau = 2*pi
