ee5c20a1fd4ed826a0fcf4c72626076f8b57e2f2
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2018-02-12 01:31:49 +0100

    Supposedly more stable euler-angle decomposition.
    Unfortunately, this fails even more tests...

--- Math/Rotations/Class.hs
+++ Math/Rotations/Class.hs
@@ -51,15 +51,46 @@ rotateViaEulerAnglesYZ yRot zRot (ℝP² rax φax) = rotAroundAxis
               r₂₁ = one_cosθ*e₂*e₁ + e₀*sinθ
               r₂₂ = one_cosθ*e₂^2  + cosθ
 
-         -- https://www.geometrictools.com/Documentation/EulerAngles.pdf
-              ( θy
-               ,θz₀
-               ,θz₁ )
-               | not (r₂₂ < 1)   = (0, atan2 r₁₀ r₁₁, 0)
-               | not (r₂₂ > -1)  = (pi, -atan2 r₁₀ r₁₁, 0)
-               | otherwise   = ( acos r₂₂
-                               , atan2 r₁₂ r₀₂
-                               , atan2 r₂₁ (-r₂₀) )
+         -- Rotation matrix for z₀-y-z₁ rotation, with cy := cos θy etc.:
+         --
+         -- ⎛ r₀₀ r₀₁ r₀₂ ⎞   ⎛ cz₁ -sz₁ 0 ⎞   ⎛ cy  0 -sy ⎞   ⎛ cz₀ -sz₀ 0 ⎞
+         -- ⎜ r₁₀ r₁₁ r₁₂ ⎟ = ⎜ sz₁  cz₁ 0 ⎟ · ⎜ 0   1  0  ⎟ · ⎜ sz₀  cz₀ 0 ⎟
+         -- ⎝ r₂₀ r₂₁ r₂₂ ⎠   ⎝ 0    0   1 ⎠   ⎝ sy  0  cy ⎠   ⎝ 0    0   1 ⎠
+         --
+         --      ⎛ cz₁ -sz₁ 0 ⎞   ⎛ cy·cz₀ -cy·sz₀ -sy ⎞
+         --    = ⎜ sz₁  cz₁ 0 ⎟ · ⎜  sz₀     cz₀    0  ⎟
+         --      ⎝  0    0  1 ⎠   ⎝ sy·cz₀    0     cy ⎠
+         --
+         --      ⎛ cy·cz₀·cz₁−sz₀·sz₁  -cz₁·sz₀−cy·cz₀·sz₁   sy·cz₀ ⎞
+         --    = ⎜ cy·cz₁·sz₀+cz₀·sz₁   cz₀·cz₁−cy·sz₀·sz₁   sy·sz₀ ⎟
+         --      ⎝      -sy·cz₁               sy·sz₁           cy   ⎠
+         --
+         -- Here, one can immediately read off
+              cy = r₂₂
+         -- ...but the naïve choice
+         --    θy = acos cy = acos r₂₂
+         -- is unstable. Better:
+         --  sqrt(r₂₀²+r₂₁²) = |sy|·(cz₁²+sz₁²) = sy
+              sy = sqrt $ r₂₀^2+r₂₁^2
+              θy = atan2 sy cy
+         -- We can always choose
+         --   θz₀ = atan2 sz₀ cz₀
+         --       = atan2 (sy·sz₀) (sy·cz₀)  ∀sy‡0
+              θz₀ = atan2 r₁₂ r₀₂  ; sz₀ = sin θz₀; cz₀ = cos θz₀
+         -- ...noting however that this becomes underconstrained for small |sy|, so
+         -- the analogous θz₁ = atan2 r₂₁ (-r₂₀) should /not/ be used. Instead, put
+         -- in the x unit vector turned back by θy and θz₀:
+         -- ⎛ r₀₀ r₀₁ r₀₂ ⎞   ⎛ cy·cz₀ -cy·sz₀ -sy ⎞⁻¹  ⎛1⎞   ⎛ cz₁ -sz₁ 0 ⎞⎛1⎞   ⎛cz₁⎞
+         -- ⎜ r₁₀ r₁₁ r₁₂ ⎟ · ⎜  sz₀     cz₀    0  ⎟  $ ⎜0⎟ = ⎜ sz₁  cz₁ 0 ⎟⎜0⎟ = ⎜sz₁⎟
+         -- ⎝ r₂₀ r₂₁ r₂₂ ⎠   ⎝ sy·cz₀    0     cy ⎠    ⎝0⎠   ⎝  0    0  1 ⎠⎝0⎠   ⎝ 0 ⎠
+         --
+         -- Here we have, using orthogonality,
+         -- ⎛ cy·cz₀ -cy·sz₀ -sy ⎞⁻¹⎛1⎞   ⎛  cy·cz₀  sz₀ sy·cz₀ ⎞⎛1⎞   ⎛ cy·cz₀ ⎞
+         -- ⎜  sz₀     cz₀    0  ⎟  ⎜0⎟ = ⎜ -cy·sz₀  cz₀   0    ⎟⎜0⎟ = ⎜-cy·sz₀ ⎟
+         -- ⎝ sy·cz₀    0     cy ⎠  ⎝0⎠   ⎝   -sy     0    cy   ⎠⎝0⎠   ⎝  -sy   ⎠
+         -- 
+              θz₁ = atan2 (r₁₀*cy*cz₀ - r₁₁*cy*sz₀ - r₁₂*sy)
+                          (r₀₀*cy*cz₀ - r₀₁*cy*sz₀ - r₀₂*sy)
        
        θax = pi/2 * rax
        e₀ = cos φax * sin θax
