5fab8f93b5e074f128471e385d982ba1ed84a75c
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2018-03-12 12:45:44 +0100

    `manifolds` package has disambiguated its constructor names.
    https://github.com/leftaroundabout/manifolds/commit/c6e218dce0a441ba2c8f6d69857625af2d010703

--- Math/Rotations/Class.hs
+++ Math/Rotations/Class.hs
@@ -28,14 +28,14 @@ class Rotatable m where
 
 instance (Rotatable m) => Rotatable (m -> Double) where
   type AxisSpace (m -> Double) = AxisSpace m
-  rotateAbout ax (S¹ δφ) f = f . rotateAbout ax (S¹ $ -δφ)
+  rotateAbout ax (S¹Polar δφ) f = f . rotateAbout ax (S¹Polar $ -δφ)
 
 instance Rotatable S¹ where
   type AxisSpace S¹ = ℝP⁰
-  rotateAbout ℝPZero (S¹ δφ) (S¹ φ)
-    | φ' > pi    = S¹ $ φ'-tau
-    | φ' < -pi   = S¹ $ φ'+tau
-    | otherwise  = S¹ φ'
+  rotateAbout ℝPZero (S¹Polar δφ) (S¹Polar φ)
+    | φ' > pi    = S¹Polar $ φ'-tau
+    | φ' < -pi   = S¹Polar $ φ'+tau
+    | otherwise  = S¹Polar φ'
    where φ' = φ + δφ
 
 rotateViaEulerAnglesYZ
@@ -44,13 +44,13 @@ rotateViaEulerAnglesYZ
   -> (ℝP² -> S¹ -> m -> m) -- ^ Suitable definition for 'rotateAbout'
 rotateViaEulerAnglesYZ yRot zRot ax = rotAroundAxis . rotmatrixForAxis ax
  where rotAroundAxis mat = case eulerAnglesZYZForMatrix mat of
-          [θz₀, θy, θz₁] -> zRot (S¹ θz₁) . yRot (S¹ θy) . zRot (S¹ θz₀)
+          [θz₀, θy, θz₁] -> zRot (S¹Polar θz₁) . yRot (S¹Polar θy) . zRot (S¹Polar θz₀)
 
 rotmatrixForAxis :: ℝP² -> S¹ -> [[ℝ]]
-rotmatrixForAxis (ℝP² rax φax) = rotAroundAxis
- where rotAroundAxis (S¹ θ) = [[r₀₀,r₀₁,r₀₂]
-                              ,[r₁₀,r₁₁,r₁₂]
-                              ,[r₂₀,r₂₁,r₂₂]]
+rotmatrixForAxis (UnitDiskℝP²Polar rax φax) = rotAroundAxis
+ where rotAroundAxis (S¹Polar θ) = [[r₀₀,r₀₁,r₀₂]
+                                   ,[r₁₀,r₁₁,r₁₂]
+                                   ,[r₂₀,r₂₁,r₂₂]]
          -- https://en.wikipedia.org/w/index.php?title=Rotation_formalisms_in_three_dimensions&oldid=823164970#Rotation_matrix_%E2%86%94_Euler_axis/angle
         where r₀₀ = one_cosθ*e₀^2  + cosθ
               r₀₁ = one_cosθ*e₀*e₁ - e₂*sinθ
@@ -127,21 +127,22 @@ eulerAnglesZYZForMatrix [[r₀₀,r₀₁,r₀₂]
 instance Rotatable S² where
   type AxisSpace S² = ℝP²
   rotateAbout = rotateViaEulerAnglesYZ
-       (\(S¹ β) (S² θ φ)
+       (\(S¹Polar β) (S²Polar θ φ)
            -> let x₀ = cos φ * sin θ
                   y  = sin φ * sin θ
                   z₀ = cos θ
                   x₁ =  x₀ * cos β + z₀ * sin β
                   z₁ = -x₀ * sin β + z₀ * cos β
                   rxy = sqrt $ x₁^2 + y^2
-              in S² (atan2 rxy z₁) (atan2 y x₁) )
-       (\γ (S² θ φ) -> case rotateAbout ℝPZero γ (S¹ φ) of S¹ φ' -> S² θ φ')
+              in S²Polar (atan2 rxy z₁) (atan2 y x₁) )
+       (\γ (S²Polar θ φ) -> case rotateAbout ℝPZero γ (S¹Polar φ) of
+                              S¹Polar φ' -> S²Polar θ φ')
 
 rotateX, rotateY, rotateZ :: (Rotatable m, AxisSpace m ~ ℝP²)
            => S¹ -> m -> m
-rotateX = rotateAbout $ ℝP² 1 0
-rotateY = rotateAbout $ ℝP² 1 (pi/2)
-rotateZ = rotateAbout $ ℝP² 0 0
+rotateX = rotateAbout $ UnitDiskℝP²Polar 1 0
+rotateY = rotateAbout $ UnitDiskℝP²Polar 1 (pi/2)
+rotateZ = rotateAbout $ UnitDiskℝP²Polar 0 0
 
 tau :: ℝ
 tau = 2*pi
